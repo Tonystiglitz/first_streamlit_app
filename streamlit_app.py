4fe3d85844457fe3da70c54bafe8508426daba3b
Tonystiglitz <158738683+Tonystiglitz@users.noreply.github.com>
2024-02-03 20:20:29 +0530

    Update streamlit_app.py
    testing with my_soil_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,3 +12,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
+
+
+my_soil_list = pandas.read_tsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/LU_SOIL_TYPE.tsv")
+streamlit.dataframe(my_soil_list)
