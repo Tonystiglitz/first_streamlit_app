15a5ce86c078374045c15a208ad09105fdecc0a3
Tonystiglitz <158738683+Tonystiglitz@users.noreply.github.com>
2024-02-03 21:50:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,9 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
-
+fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 # my_soil_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/LU_SOIL_TYPE.tsv",sep='\t', header=0)
 # streamlit.dataframe(my_soil_list)
